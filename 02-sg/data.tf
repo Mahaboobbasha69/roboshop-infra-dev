9936733a7cca2b3d7d8479471f392c5d5026c959
myusername <myemail>
2026-01-13 10:56:36 +0530

    Commiting terraform

--- 02-sg/data.tf
+++ 02-sg/data.tf
@@ -5,3 +5,14 @@ data "aws_ssm_parameter" "vpc_id" {
 data "aws_vpc" "default" {
   default = true
 }
+
+
+
+# Purpose:
+# Reads existing VPC details
+
+# Does NOT create anything
+
+# Used only for reference
+
+# 👉 Think of data as read-only
